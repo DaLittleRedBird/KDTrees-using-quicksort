cbef080c794e637571dd705ee1a44482125c9b32
DaLittleRedBird <107221162+DaLittleRedBird@users.noreply.github.com>
2022-08-07 20:56:33 -0400

    Update KDTreeQuicksort.js

--- KDTreeQuicksort.js
+++ KDTreeQuicksort.js
@@ -1,7 +1,4 @@
-/*
-NOTE : NONE OF THIS has been rigourously bug tested yet, if you intend to fork any of this, then make sure you rigourously bug test/fix it 
-first before using it for any projects of your own.
-*/
+function swap(array, a, b) { const temp = array[a]; array[a] = array[b]; array[b] = temp; }
 
 // Returns the k-th smallest element of list within left...right inclusive (i.e. left <= k <= right).
 function quickselect(array, hi, low, k) {
@@ -15,81 +12,63 @@ function quickselect(array, hi, low, k) {
     return low;
 }
 
-/*
-The axis variable tells the algorithm how it should sort the array :
-0 - 1 dimensional array that uses objects without (x, y, z) coordinates.
-1 - x-axis.
-2 - y-axis.
-3 - z-axis.
-*/
-function getpivot(array, hi, low, axis) {
-    if (hi - low < 5) { return medianAtmost5(array, hi, low); }
-    let i, subRight, mid, median5, temp;
+function getpivot(array, hi, low) {
+    if (hi - low < 5) { return medianAtmost5(array, hi, low, 0); }
+    let i, subRight, mid, median5;
     for (i = low; i < hi; i += 5) {
         subRight = i + 4;
         if (subRight > hi) { subRight = hi; }
-        median5 = medianAtmost5(list, i, subRight);
-        temp = array[median5];
-        array[median5] = array[low + Math.floor((i - low)/5)];
-        array[low + Math.floor((i - low)/5)] = temp;
+        median5 = medianAtmost5(array, i, subRight, 0);
+        swap(array, median5, low + Math.floor((i - low)/5));
     }
     mid = Math.floor((hi - low) / 10) + low + 1;
     return quickselect(array, low, low + Math.floor((hi - low) / 5), mid);
 }
 
 function partition(array, hi, low, pivotIdx, axis, quickalgPair) {
-    let pivotValue = array[pivotIdx], temp, j, storeIndex, storeIndexEq;
+    let j, storeIndex, storeIndexEq, inOrder;
     // Move pivot to end
-    array[pivotIdx] = array[hi];
-    array[hi] = pivotValue;
+    let pivotValue = array[pivotIdx]; array[pivotIdx] = array[hi]; array[hi] = pivotValue;
     storeIndex = low;
     // Move all elements smaller than the pivot to the left of the pivot
     for (j = low; j < hi; j++) {
-        if (array[j] < pivotValue) {
-            temp = array[storeIndex];
-            array[storeIndex] = array[j];
-            array[j] = temp;
-            storeIndex++;
-        }
+        inOrder = axis == 0 ? array[j] < pivotValue : axis == 1 ? array[j].x < pivotValue.x : axis == 2 ? array[j].y < pivotValue.y : array[j].z < pivotValue.z;
+        if (inOrder) { swap(array, storeIndex, j); storeIndex++; }
     }
     // Move all elements equal to the pivot right after the smaller elements
     storeIndexEq = storeIndex;
     for (j = storeIndex; j < hi; j++) {
-        if (array[j] == pivotValue) {
-            temp = array[storeIndex];
-            array[storeIndex] = array[j];
-            array[j] = temp;
-            storeIndexEq++;
-        }
+        inOrder = axis == 0 ? array[j] == pivotValue : axis == 1 ? array[j].x == pivotValue.x : axis == 2 ? array[j].y == pivotValue.y : array[j].z == pivotValue.z;
+        if (inOrder) { swap(array, storeIndexEq, j); storeIndexEq++; }
     }
     // Move the pivot to where it belongs
-    temp = array[storeIndexEq];
-    array[storeIndexEq] = array[j];
-    array[j] = temp;
+    swap(array, storeIndexEq, hi);
+    
     //Is this quicksort? If so, return the bounds of the partition
-    if (quickalgPair[0]) { return [storeIndex, storeIndexEq]; }
+    if (quickalgPair[0]) {return [storeIndex, storeIndexEq];}
+    
     // If not, return location of pivot considering the desired location quickalgPair[1]
     if (quickalgPair[1] < storeIndex) {return storeIndex;}  // quickalgPair[1] is in the group of smaller elements
     if (quickalgPair[1] <= storeIndexEq) {return quickalgPair[1];}  // quickalgPair[1] is in the group equal to pivot
     return storeIndexEq; // quickalgPair[1] is in the group of larger elements
 }
 
-function medianAtmost5(array, hi, low) {
-    let i, j, temp;
+function medianAtmost5(array, hi, low, axis) {
+    let i, j, inOrder;
     switch (hi - low) {
-        case 5:
-        case 4:
-        i = low + 1;
-        while (i <= hi) {
-            for (j = i; j > low && array[j - 1] > array[j]; j--) {
-                temp = array[j - 1];
-                array[j - 1] = array[j];
-                array[j] = temp;
-            }
-            i++;
+        case 5: case 4:
+        for (i = low + 1; i <= hi; i++) {
+            inOrder = axis == 0 ? array[j - 1] > array[j] : axis == 1 ? array[j - 1].x > array[j].x : axis == 2 ? array[j - 1].y > array[j].y : array[j - 1].z > array[j].z;
+            for (j = i; j > low && inOrder; j--) {
+                swap(array, j - 1, j);
+                inOrder = axis == 0 ? array[j - 1] > array[j] : axis == 1 ? array[j - 1].x > array[j].x : axis == 2 ? array[j - 1].y > array[j].y : array[j - 1].z > array[j].z;
+            } 
         }
-        return Math.floor((low + hi) / 2);
-        case 3: const InOrder = array[low + 1] >= array[low]; const Max2lowest = (InOrder ? low : low + 1), Min2lowest = (InOrder ? low + 1 : low); return array[hi] >= Max2lowest ? Max2lowest : (array[hi] >= Min2lowest ? hi : Min2lowest);
+        return Math.floor((hi + low) / 2);
+        
+        case 3: inOrder = array[low + 1] >= array[low]; const Max2lowest = (inOrder ? low : low + 1), Min2lowest = (inOrder ? low + 1 : low);
+        return array[hi] >= Max2lowest ? Max2lowest : (array[hi] >= Min2lowest ? hi : Min2lowest);
+        
         case 2: return array[hi] >= array[low] ? low : hi;
         default: return low;
     }
@@ -112,8 +91,10 @@ function kdnode(shape) {
     this.query = function(range, found) {
         if (!found) { found = []; }
         if (range.intersects(this.shape)) { found.push(this.shape); }
+        
         if (this.left) { this.left.query(range, found); }
         if (this.right) { this.right.query(range, found); }
+        
         return found;
     }
 }
@@ -121,9 +102,11 @@ function kdnode(shape) {
 //A 3d k-d tree constructor
 function constructkdtree(pointLst, hi, low, axis) {
     if (low >= hi || low < 0) {return;}
+    
     // Sort point list and choose median as pivot element
     let medianIndex = getpivot(pointLst, hi, low, axis), node = new kdNode(pointLst[medianIndex]);
     let quickpartition = partition(pointLst, hi, low, medianIndex, axis, [true]), left = quickpartition[0], right = quickpartition[1];
+    
     // Create node and construct subtree
     node.shape = pointLst[medianIndex];
     node.left = constructkdtree(pointLst, low, left - 1, (axis + 1) % 3 + 1);
@@ -133,18 +116,23 @@ function constructkdtree(pointLst, hi, low, axis) {
 
 function getdist(point) { return point.x * point.x + point.y * point.y + point.z * point.z; }
 
+//Unfinished
 function search(tree, point, pointLst, depth, nearestNghbor) {
     let best = nearestNghbor, bestDist, curDist, axis, diff, close, away;
     if (!tree) { return nearestNeighbor; }
+    
     bestDist = getdist(nearestNghbor); curDist = getdist(point);
     if (!best || curDist < bestDist) { best = point; }
+    
     axis = depth % 3; diff = point[axis] - tree.value[axis];
+    
     if (diff <= 0) { close = tree.left; away = tree.right; } else { close = tree.right; away = tree.left; }
     best = search(close, point, pointLst, depth + 1, best);
     if (diff * diff < best.distance) { best = search(away, point, pointLst, depth + 1, best); }
     return best;
 }
 
+//Somewhat unfinished
 function findNearestNeighbor(point, pointLst) {
     var tree = constructkdtree(points, points.length, 0, 1);
     const nearestNghbor = search(tree, point, pointLst, 1, null);
